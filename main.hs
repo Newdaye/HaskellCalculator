5d81b99d043b0ff8c19c176924cfd77db044b1db
Riley Weasel Fat <riley.weaselfat@uleth.ca>
2020-03-25 17:11:37 -0600

    Calculator Functional

--- main.hs
+++ main.hs
@@ -1,17 +1,13 @@
 import Control.Monad
 
 main = do
+    putStr "> "
     input <- getLine
     unless(input=="q") $ do
         let n = rpn input
-        if 
         print n
-        else
         main
 
-parseText :: String -> Bool
-parseText [] = []
-parseText 
 
 rpn :: String -> Double
 rpn [] = 0
@@ -22,9 +18,23 @@ rpn' (x:y:ys) "*" = (x*y):ys
 rpn' (x:y:ys) "-" = (x-y):ys
 rpn' (x:y:ys) "+" = (x+y):ys
 rpn' (x:y:ys) "/" = (y/x):ys
-rpn' (x:y:ys) "^" = (y**x):ys
+rpn' (x:y:ys) "^" = (y**x):ysa
 rpn' (x:xs) "ln" = log x:xs
+rpn' (x:xs) "!" = fac x:xs
+rpn' (x:xs) "cos" = cos x:xs
+rpn' (x:xs) "sin" = sin x:xs
+rpn' (x:xs) "tan" = tan x:xs
+rpn' (x:xs) "acos" = acos x:xs
+rpn' (x:xs) "asin" = asin x:xs
+rpn' (x:xs) "atan" = atan x:xs
+rpn' (x:xs) "asinh" = asinh x:xs
+rpn' (x:xs) "atanh" = atanh x:xs
+rpn' (x:xs) "acosh" = acosh x:xs
+rpn' (x:xs) "log" = log x:xs
+rpn' (x:xs) "sqrt" = sqrt x:xs
 rpn' ys n = (read n):ys
 
 
--- Couldn't match expected type ‘IO a0’ with actual type ‘Double’
+fac :: Double -> Double
+fac 0 = 1
+fac n = n * fac (n-1)
