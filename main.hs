2e8f6f963d081613416b91aa274f572e364946af
Riley Weasel Fat <riley.weaselfat@uleth.ca>
2020-03-25 15:30:51 -0600

    added more funcitonality

--- main.hs
+++ main.hs
@@ -1,8 +1,30 @@
-
+import Control.Monad
 
 main = do
-    -- input = putstrln
-    --parse input
-    -- deal with input
-    -- output 
-    main
+    input <- getLine
+    unless(input=="q") $ do
+        let n = rpn input
+        if 
+        print n
+        else
+        main
+
+parseText :: String -> Bool
+parseText [] = []
+parseText 
+
+rpn :: String -> Double
+rpn [] = 0
+rpn x = head $ foldl rpn' [] $ words x
+
+rpn' :: [Double] -> String -> [Double]
+rpn' (x:y:ys) "*" = (x*y):ys
+rpn' (x:y:ys) "-" = (x-y):ys
+rpn' (x:y:ys) "+" = (x+y):ys
+rpn' (x:y:ys) "/" = (y/x):ys
+rpn' (x:y:ys) "^" = (y**x):ys
+rpn' (x:xs) "ln" = log x:xs
+rpn' ys n = (read n):ys
+
+
+-- Couldn't match expected type ‘IO a0’ with actual type ‘Double’
